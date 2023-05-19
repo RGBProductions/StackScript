6a169e521eab36ab32c802157e8aa60aa671eb72
RGBCube <luvm1n3craft@gmail.com>
2023-05-19 11:26:23 -0700

    Finished main script

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
 import sys
+import subprocess
 
-path = None
+script = None
 run = True
 
-for arg in sys.argv:
-    if arg == "-c":
-        run = False
-    else:
-        if path == None:
-            path = arg
+base = os.path.dirname(__file__)
+
+for arg in sys.argv[1:]:
+	if arg == "-c":
+		run = False
+	else:
+		if script == None:
+			script = arg
+
+if script == None:
+	print("E: no script path provided")
+	exit()
+
+subprocess.run([sys.executable, os.path.join(base, "compile.py"), script])
+if run:
+	subprocess.run([sys.executable, os.path.join(base, "interpreter.py"), "out/compiled.stksc"])
